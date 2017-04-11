15a455cc5115bf6722f326e87bdd4b02a5704f04
Thy Dai <kathydai@email.uark.edu>
2017-04-11 08:06:07 -0500

    Updated GUI
    Let me know if this doesn't work... I had to manually add in Brett's
    changes

--- src/main/Main.java
+++ src/main/Main.java
@@ -18,7 +18,6 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hello World");
         Controller cont = new Controller();
         cont.startApplication();
     }
