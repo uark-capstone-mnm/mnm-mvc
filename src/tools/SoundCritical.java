56136661eedba9d406e3d55746792795eddc4d8f
Brett <badixon@uark.edu>
2017-04-26 13:53:42 -0500

    Initial work on making warning sounds managable and stored within the ini file

--- src/tools/SoundCritical.java
+++ src/tools/SoundCritical.java
@@ -1,41 +1,38 @@
 package tools;
 
 import javax.sound.sampled.*;
+import models.Configuration;
 
 public class SoundCritical extends Thread {
 
-  public void run() {
-    SoundCritical.alert();
-  }
+	public void run() {
+		SoundCritical.alert();
+	}
 
-  public static void alert() {
-    int repeat = 10;
-    try {
-      AudioFormat af = new AudioFormat(8000f, 8, 1, true, false);
-      SourceDataLine sdl;
-      sdl = AudioSystem.getSourceDataLine(af);
-      sdl.open(af);
-      sdl.start();
+	public static void alert() {
+		int repeat = Configuration.getCriticalSoundRepeat();
+		try {
+			AudioFormat af = new AudioFormat(8000f, 8, 1, true, false);
+			SourceDataLine sdl;
+			sdl = AudioSystem.getSourceDataLine(af);
+			sdl.open(af);
+			sdl.start();
 
-      byte[] buffer = new byte[1];
-      int step;
+			byte[] buffer = new byte[1];
 
-      for (int j = 0; j < repeat; j++) {
-        step = 10;
-        for (int i = 0; i < 2000; i++) {
-          buffer[0] = ((i % step > 0) ? 32 : (byte) 0);
-
-          if (i % 250 == 0)
-            step += 2;
-          sdl.write(buffer, 0, 1);
-        }
-        Thread.sleep(200);
-      }
-      sdl.drain();
-      sdl.stop();
-      sdl.close();
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-  }
+			for (int j = 0; j < repeat; j++) {
+				for (int i = 0; i < 1600; i++) {
+					double angle = i / (8000f / Configuration.getCriticalSoundPitch() ) * 2.0 * Math.PI;
+					buffer[0] = (byte)(Math.sin(angle) * 127.0);
+					sdl.write(buffer,0,1);
+				}
+				Thread.sleep(Configuration.getCriticalPause());
+			}
+			sdl.drain();
+			sdl.stop();
+			sdl.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
--- src/tools/SoundWarning.java
+++ src/tools/SoundWarning.java
@@ -1,37 +1,38 @@
 package tools;
 
 import javax.sound.sampled.*;
+import models.Configuration;
 
 public class SoundWarning extends Thread {
 
-  public void run() {
-    SoundWarning.alert();
-  }
+	public void run() {
+		SoundWarning.alert();
+	}
 
-  public static void alert() {
-    int repeat = 2;
-    try {
-      AudioFormat af = new AudioFormat(8000f, 8, 1, true, false);
-      SourceDataLine sdl;
-      sdl = AudioSystem.getSourceDataLine(af);
-      sdl.open(af);
-      sdl.start();
+	public static void alert() {
+		int repeat = Configuration.getWarningRepeat();
+		try {
+			AudioFormat af = new AudioFormat(8000f, 8, 1, true, false);
+			SourceDataLine sdl;
+			sdl = AudioSystem.getSourceDataLine(af);
+			sdl.open(af);
+			sdl.start();
 
-      byte[] buffer = new byte[1];
+			byte[] buffer = new byte[1];
 
-      for (int j = 0; j < repeat; j++) {
-        for (int i = 0; i < 1600; i++) {
-        	double angle = i / (8000f / 1000) * 2.0 * Math.PI;
-            buffer[0] = (byte)(Math.sin(angle) * 127.0);
-            sdl.write(buffer,0,1);
-        }
-        Thread.sleep(350);
-      }
-      sdl.drain();
-      sdl.stop();
-      sdl.close();
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-  }
+			for (int j = 0; j < repeat; j++) {
+				for (int i = 0; i < 1600; i++) {
+					double angle = i / (8000f / Configuration.getWarningSoundPitch() ) * 2.0 * Math.PI;
+					buffer[0] = (byte)(Math.sin(angle) * 127.0);
+					sdl.write(buffer,0,1);
+				}
+				Thread.sleep(Configuration.getWarningPause());
+			}
+			sdl.drain();
+			sdl.stop();
+			sdl.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
