f51f1c5293b38ced8c4e8609384d7260258d4baa
Kylie McClanahan <kylielmcclanahan@gmail.com>
2017-05-01 16:17:20 -0500

    I'm not sure what I changed
    but I'm pretty sure it's nothing major

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -93,7 +93,7 @@ public class Controller {
         }
     }
     
-    public static int getGlobalOxygenation() {
+    public int getGlobalOxygenation() {
         Double sum = 0.0;
         for(int i = 0; i < oxygenation.size(); i++) {
             sum += oxygenation.get(i);
